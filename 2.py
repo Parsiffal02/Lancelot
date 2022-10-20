8d08e2ef974b69fe93833bc41b075897442b8b54
Parsiffal02 <mr.gorge1972@mail.ru>
2022-10-20 11:55:24 +0300

    обавил проверку на дурака

--- 2.py
+++ 2.py
@@ -1,6 +1,11 @@
 import math 
 def area_of_the_circle(Radius):  
     area = Radius** 2 * math.pi 
-    return area  
-Radius = float(input("Пожалуйста, введите радиус окружности: ")) 
+    return area
+while True:                                        #Проверка на дурака
+    try:
+        Radius = float(input("Пожалуйста, введите радиус окружности: "))                              
+        break
+    except ValueError:
+        print('Вы должны ввести число! Попробуйте снова!')
 print(" Площадь окружности равна: ", area_of_the_circle(Radius))
